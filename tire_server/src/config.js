87afa8130f40ae6615d31c778406282ade05a74d
Yunys26 <aliev-unys@rambler.ru>
2021-03-12 09:56:56 +0300

    Серверная шина

--- tire_server/src/config.js
+++ tire_server/src/config.js
@@ -12,10 +12,10 @@ module.exports ={
     },
     bus:{
         brigade: "25",
-        init: {"from":`25`,"to":"dean","subject":"INIT_INSTANCE","data":null},
-        subscribe: {"from":`25`,"to":"dean","subject":"UPDATE_SUBSCRIPTION","data":"{\"25\":\"testuser\",\"entityName\":\"student\",\"type\":\"COMMON\"}"},
+        init: {"from":`25`,"to":"150","subject":"INIT_INSTANCE","data":null},
+        subscribe: {"from":`25`,"to":"150","subject":"UPDATE_SUBSCRIPTION","data":"{\"25\":\"testuser\",\"entityName\":\"student\",\"type\":\"COMMON\"}"},
         create_post: function(id, surname, name, second_name, study_group_id){
-            return {"from":"25","to":"dean","subject":"ADD_ROW",
+            return {"from":"25","to":"150","subject":"ADD_ROW",
             "data":`{\"isBinariesChanged\":false,\"entityName\":\"student\",\"plainData\":{\"id\":${id},\"surname\":\"${surname}\",\"name\":\"${name} \",\"second_name\":\"${second_name} \",\"study_group_id\":${Number(study_group_id)},\"study_group\":null},\"binaryLinks\":{}}`}}
     }
 }
